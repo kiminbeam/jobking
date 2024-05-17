eda42258afb63eeb9b60dac1c6df9cd924ba9a47
kiminbeom <kods9852@naver.com>
2024-05-17 09:10:15 +0900

    프로젝트 기본설정 완료

---
+++ jobking/src/main/java/com/example/jobking/JobkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobkingApplication.class, args);
+	}
+
+}
