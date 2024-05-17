4f316f9a548c72b29234f0dcc361aea5620c627a
Jia Yoo <jiayoo.dev@gmail.com>
2024-05-17 12:07:35 +0900

    login_form 화면 구현

--- jobking/src/main/java/com/example/jobking/controller/JController.java
+++ jobking/src/main/java/com/example/jobking/controller/JController.java
@@ -3,12 +3,30 @@ package com.example.jobking.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
+
 public class JController {
 
 	
 	@RequestMapping("/")
 	public String root() {
 		return "index";
+	}
+	
+	@RequestMapping("/login_form")
+	public void loginForm() {
+		
+	}
+	@RequestMapping("/user_login")
+	public void userLogin(HttpServletRequest request) {
+//		String uid = request.getParameter("uid");
+//		String upw = request.getParameter("upw");
+//		System.out.println("xxx");
+	}
+	@RequestMapping("/company_login")
+	public void companyLogin() {
+		
 	}
 }
