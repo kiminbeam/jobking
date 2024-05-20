af33c3edc7bcb5b17c6b18c7b2e3b3c36873e0ee
Jia Yoo <jiayoo.dev@gmail.com>
2024-05-20 17:56:37 +0900

    포지션 제안 리스트페이지와 상세페이지 구현

--- jobking/src/main/java/com/example/jobking/repository/IOfferListRepository.java
+++ jobking/src/main/java/com/example/jobking/repository/IOfferListRepository.java
@@ -1,9 +1,16 @@
 package com.example.jobking.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.jobking.entity.OfferList;
 
 public interface IOfferListRepository extends JpaRepository<OfferList, Long> {
 
+	
+	@Query(value="select * from offer_list where uid= :uid", nativeQuery=true)
+	List<OfferList> findAllByUid(@Param("uid") String uid);
 }
