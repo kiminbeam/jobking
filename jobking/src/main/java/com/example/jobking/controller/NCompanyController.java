b8138e14a0dc5ab52e97dfcfde8a399f1c3d7933
nyoung <ara347398@gmail.com>
2024-05-22 01:09:55 +0900

    구직자 대표 이력서 목록 출력 기능 구현 완료. /  스크랩, 이력서 확인, 포지션 제안 버튼 기능 구현 필요

--- jobking/src/main/java/com/example/jobking/controller/NCompanyController.java
+++ jobking/src/main/java/com/example/jobking/controller/NCompanyController.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.jobking.entity.Career;
+import com.example.jobking.entity.Hope;
 import com.example.jobking.entity.Resume;
 import com.example.jobking.entity.User;
+import com.example.jobking.repository.ICareerRepository;
 import com.example.jobking.repository.ICompanyRepository;
+import com.example.jobking.repository.IHopeRepository;
 import com.example.jobking.repository.IJobAdRepository;
 import com.example.jobking.repository.IResumeRepository;
 import com.example.jobking.repository.IUserRepository;
@@ -29,20 +33,35 @@ public class NCompanyController {
 	@Autowired
     private IUserRepository userRepository;
 
+	
+	@Autowired
+	private ICareerRepository careerRepository;
+	
     @Autowired
     private IResumeRepository resumeRepository;
+    
+    @Autowired
+    private IHopeRepository hopeRepository;
 	
 	
 	@RequestMapping("com_totalfind")
 	public String totalFind(Model model) {
 		
 		List<User> allUsers = userRepository.findAll(); // 모든 유저 조회
+		List<Hope> defaultHopes = new ArrayList<>();
         List<Resume> defaultResumes = new ArrayList<>();
+        List<List<Career>> defaultCareers = new ArrayList<>(); // 각 이력서에 대한 경력 목록 리스트
 
         for (User user : allUsers) {
             Resume defaultResume = resumeRepository.findByUserAndDef(user, "1");
             if (defaultResume != null) {
+            	Hope defaultHope = hopeRepository.findByUserAndResume(user, defaultResume);
+            	defaultHopes.add(defaultHope);
                 defaultResumes.add(defaultResume); // 대표 이력서만 추가
+                
+             // 해당 이력서의 경력 목록 조회
+                List<Career> careers = careerRepository.findByResume(defaultResume);
+                defaultCareers.add(careers); // 경력 목록 리스트에 추가
             }
         }
         
@@ -50,7 +69,8 @@ public class NCompanyController {
         model.addAttribute("defaultResumeCount", defaultResumeCount);
 
         model.addAttribute("defaultResumes", defaultResumes);
-		
+        model.addAttribute("defaultHopes", defaultHopes);
+        model.addAttribute("defaultCareers", defaultCareers);
 		
 		return "company/com_totalfind";
 	}
--- jobking/src/main/java/com/example/jobking/repository/IHopeRepository.java
+++ jobking/src/main/java/com/example/jobking/repository/IHopeRepository.java
@@ -3,7 +3,12 @@ package com.example.jobking.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.jobking.entity.Hope;
+import com.example.jobking.entity.Resume;
+import com.example.jobking.entity.User;
 
 public interface IHopeRepository extends JpaRepository<Hope, Long> {
 
+	
+	// 유저와 이력서를 기준으로 희망 조건 조회
+    Hope findByUserAndResume(User user, Resume resume);
 }
