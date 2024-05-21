681f92c4d53ddeb39c6a59d983b320bd5b85cd56
kiminbeom <kods9852@naver.com>
2024-05-21 18:43:04 +0900

    리파지토리 쿼리 추가(회사리뷰, 면접자 리스트, 모집공고, 유저리뷰)

--- jobking/src/main/java/com/example/jobking/repository/ICompanyReviewRepository.java
+++ jobking/src/main/java/com/example/jobking/repository/ICompanyReviewRepository.java
@@ -1,9 +1,15 @@
 package com.example.jobking.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.jobking.entity.CompanyReview;
 
 public interface ICompanyReviewRepository extends JpaRepository<CompanyReview, Long> {
-
+	
+	@Query(value="select * from company_review where cid= :cid", nativeQuery = true)
+	Optional <CompanyReview> findCpReview(@Param("cid") String cid);
 }
