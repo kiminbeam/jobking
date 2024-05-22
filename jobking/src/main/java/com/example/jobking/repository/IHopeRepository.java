6cadc2562f0b9e60e2a9de27c47bd67d619836e9
Jia Yoo <jiayoo.dev@gmail.com>
2024-05-22 11:09:46 +0900

    평균평점 추가

--- jobking/src/main/java/com/example/jobking/repository/IHopeRepository.java
+++ jobking/src/main/java/com/example/jobking/repository/IHopeRepository.java
@@ -1,8 +1,13 @@
 package com.example.jobking.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.jobking.entity.Hope;
+import com.example.jobking.entity.UserReview;
 
 public interface IHopeRepository extends JpaRepository<Hope, Long> {
 
--- jobking/src/main/java/com/example/jobking/repository/IUserReviewRepository.java
+++ jobking/src/main/java/com/example/jobking/repository/IUserReviewRepository.java
@@ -12,5 +12,9 @@ public interface IUserReviewRepository extends JpaRepository<UserReview, Long> {
 	
 	@Query(value="select * from user_review where cid = :cid", nativeQuery = true)
 	Optional<UserReview> findUserReview(@Param("cid") String cid);
+	
+	//평균 구하는 쿼리
+	@Query(value="select ((sum(q1) + sum(q2) + sum(q3))/3) / count(distinct cid) from company_review where uid= :uid", nativeQuery = true)
+	Double findAverageByUid(@Param("uid") String uid);
 
 }
