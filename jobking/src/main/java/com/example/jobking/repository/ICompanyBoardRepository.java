5b543f472ecccd8f310c0a52548e7b9d271b3a98
kiminbeom <kods9852@naver.com>
2024-05-23 11:04:27 +0900

    기업 커뮤니티페이지, 상세페이지 생성

--- jobking/src/main/java/com/example/jobking/repository/ICompanyBoardRepository.java
+++ jobking/src/main/java/com/example/jobking/repository/ICompanyBoardRepository.java
@@ -1,9 +1,18 @@
 package com.example.jobking.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.jobking.entity.CompanyBoard;
 
 public interface ICompanyBoardRepository extends JpaRepository<CompanyBoard, Long> {
-
+	
+	@Query(value="select * from company_board where type= :type",nativeQuery=true)
+	List<CompanyBoard> findAllByType(@Param("type") String type);
+	
+	@Query(value="select * from company_board where type= :type order by regdate desc limit 1", nativeQuery=true)
+	CompanyBoard findLatestBoardByType(@Param("type") String type);
 }
--- jobking/src/main/java/com/example/jobking/repository/ICompanyReplyRepository.java
+++ jobking/src/main/java/com/example/jobking/repository/ICompanyReplyRepository.java
@@ -1,9 +1,19 @@
 package com.example.jobking.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.jobking.entity.CompanyReply;
 import com.example.jobking.entity.CompanyReview;
 
 public interface ICompanyReplyRepository extends JpaRepository<CompanyReview, Long> {
 
+	@Query(value="delete from company_reply where cbno= :cbno",nativeQuery=true)
+	void deleteAllByCbno(@Param("cbno") Long cbno);
+	
+	@Query(value="select * from company_reply where cbno= :cbno",nativeQuery=true)
+	List<CompanyReply> findAllByCbno(@Param("cbno") Long cbno);
 }
